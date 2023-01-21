d5f6d937bb26ebe1e15f76e88c0ea53850c2a405
Ben Wilson <benjamin.wilson2@icloud.com>
2023-01-21 14:04:37 +0000

    Add animated graph and show compute time.

--- TSPbruteforce.py
+++ TSPbruteforce.py
@@ -1,9 +1,10 @@
 import numpy as np
 from itertools import permutations
 import matplotlib.pyplot as plt
+import time
 
 
-
+fig = plt.figure()
 
 def create_nodes(n):
 
@@ -11,8 +12,7 @@ def create_nodes(n):
     for i in range(n):
         node = np.round(np.random.rand(2) * 100)
         node_array.append(node)
-        plt.plot(node[0], node[1], "ro")
-        plt.text(node[0] * (1 + 0.01), node[1] * (1 + 0.01), i)
+        
 
 
     print(node_array)
@@ -30,7 +30,7 @@ def create_distance_matrix(node_array):
             distance_matrix[i][j] = distance
             distance_matrix[j][i] = distance
 
-    print(distance_matrix)
+    
     return distance_matrix
 
 def brute_force(node_array):
@@ -41,12 +41,12 @@ def brute_force(node_array):
     
     for perm in perms:
         permLen = calculateLength(distance_matrix, perm)
+        draw_graph(node_array, perm, permLen, False)
         if permLen < bestLength:
             bestLength = permLen
             bestPerm = perm
 
-    print(perms)
-    print(len(perms))
+    
 
     
     print("Best permutation = ", bestPerm)
@@ -54,37 +54,75 @@ def brute_force(node_array):
     return bestPerm, bestLength
 
 def calculateLength(distance_matrix, permutation):
-    print("current perm: ",permutation)
+    #print("current perm: ",permutation)
     sum = 0
 
     for i in range(len(permutation)):
         x = i % len(permutation)
         y = (i+1) % len(permutation)
 
-        print(permutation[x])
         sum += distance_matrix[permutation[x], permutation[y]]
     
     return sum
 
+def draw_graph(node_array, permutation, length, endFlag):
+    
+    plt.grid()
+    
+    if endFlag == True:
+        plt.pause(0.0001)
+        plt.clf()
+        plt.grid()
 
-def main():
-    fig = plt.figure()
-
-    node_array = create_nodes(6)
-
-    bestPerm, bestLength = brute_force(create_distance_matrix(node_array))
+    if endFlag == False:
+        plt.ion()
 
     x_values = []
     y_values = []
 
-    for i in range(len(bestPerm)):
-        x_values.append(node_array[bestPerm[i]][0])
-        y_values.append(node_array[bestPerm[i]][1])
+    for i in range(len(node_array)):
+        plt.plot(node_array[i][0], node_array[i][1], "ro")
+        plt.text(node_array[i][0] * (1 + 0.03), node_array[i][1] * (1 + 0.03), i)
+
+    for i in range(len(permutation)):
+        x_values.append(node_array[permutation[i]][0])
+        y_values.append(node_array[permutation[i]][1])
     
     plt.plot(x_values, y_values, "bo-")
-    plt.title(str("Distance: "+str(bestLength)))
+    plt.title(str("Path: "+str(permutation)+"      Distance ≈ "+str(round(length, 2))))
+
+    
+
+    if endFlag == False:
+        plt.draw()
+        plt.pause(0.0001)
+        plt.clf()
+        plt.show()
+    else:
+        plt.show(block=True)
+        
+    
+
+
+
+def main():
+    
+    nodes = int(input("How many nodes should be used? "))
+    
+    node_array = create_nodes(nodes)
+
+    start = time.time()
+
+    bestPerm, bestLength = brute_force(create_distance_matrix(node_array))
+
+    end = time.time()
+
+    print("Computation time for",nodes,"nodes was", (end-start),"seconds!")
+
+    draw_graph(node_array, bestPerm, bestLength, True)
             
-    plt.grid()
-    plt.show()
+    
+    
+    
 
 main()
